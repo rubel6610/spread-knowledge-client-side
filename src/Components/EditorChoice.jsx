db3aa2b810d72538df76942bf3362f02e644cc89
MD RUBEL HOSEN <rubelhosen1310@gmail.com>
2025-06-25 21:17:35 +0600

    added editor choice components

---
+++ src/Components/EditorChoice.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+const EditorChoice = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BASEURL}/editorchoice`).then((res) => {
+      setArticles(res.data);
+    });
+  }, []);
+  return (
+    <div className="p-4 bg-gray-900 text-white mt-4">
+      <h2 className="text-2xl font-bold mb-4">Editor's Choice</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {articles.map((article) => (
+          <div key={article._id} className="bg-gray-800 p-4 rounded shadow">
+            <img
+              src={article.thumbnail}
+              alt={article.title}
+              className="w-full h-40 object-cover mb-4 rounded"
+            />
+            <h3 className="text-md font-bold mb-2">{article.title}</h3>
+            <p className="text-gray-400 mb-2">Author: {article.authorName}</p>
+            <p className="text-gray-300">{article.content.slice(0, 80)}...</p>
+             <Link to={`/all-articles/${article._id}`}
+              className="mt-2 btn btn-primary text-white w-full"
+            >
+              Read More
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EditorChoice;
